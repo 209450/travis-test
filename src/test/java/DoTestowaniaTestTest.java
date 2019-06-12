cb62166f3318eba17d68ac56294fb158c10af7b9
209450 <209450@edu.p.lodz.pl>
2019-06-12 13:03:05 +0200

    Dodanie podstawowego testu i pom

---
+++ src/test/java/DoTestowaniaTestTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoTestowaniaTestTest {
+
+    @Test void zawszeTrue() {
+        System.out.println("Bedzie zawsze true.");
+        assertTrue(true);
+    }
+}
